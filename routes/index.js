8632fbacb7c68e2b4512d07e41a11c9aeff5fe75
mina <mp922352612@gmail.com>
2019-10-30 16:45:42 +0800

    Add isAdmin to users table and authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,31 @@ const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 module.exports = (app, passport) => {
+  // authentication function
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
   // if user meet homepage, redirect to /restaurant page
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
 
   // under /restaurants, let restController.getRestaurants handles
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // handle /admin/restaurants with adminController.getRestaurants
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+
 
   // user signup
   app.get('/signup', userController.signUpPage)
@@ -20,4 +36,6 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
+
+
 }
