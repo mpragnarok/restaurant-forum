fc4918d34674b39950605a8076f8eb7374a707bc
mina <mp922352612@gmail.com>
2019-11-05 22:01:29 +0800

    Convert time format using moment

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,12 @@
 const hbs = require('handlebars')
+const moment = require('moment')
 
 hbs.registerHelper('ifCond', function(a, b, options) {
   if (a === b) {
     return options.fn(this)
   }
   return options.inverse(this)
+})
+hbs.registerHelper('moment', function(a) {
+  return moment(a).fromNow()
 })
